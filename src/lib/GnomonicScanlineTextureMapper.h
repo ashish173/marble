86066ffd9befd19928955715cd0353287d888945
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2011-01-10 10:22:28 +0100

    gnomonic

---
+++ src/lib/GnomonicScanlineTextureMapper.h
@@ -0,0 +1,54 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2011      Bernhard Beschow <bbeschow@cs.tu-berlin.de>
+//
+
+#ifndef MARBLE_GNOMONICSCANLINETEXTUREMAPPER_H
+#define MARBLE_GNOMONICSCANLINETEXTUREMAPPER_H
+
+
+#include "TextureMapperInterface.h"
+
+#include <QtCore/QThreadPool>
+#include <QtGui/QImage>
+
+#include <MarbleGlobal.h>
+
+
+namespace Marble
+{
+
+class StackedTileLoader;
+
+class GnomonicScanlineTextureMapper : public TextureMapperInterface
+{
+ public:
+    GnomonicScanlineTextureMapper( StackedTileLoader *tileLoader );
+
+    virtual void mapTexture( GeoPainter *painter,
+                             const ViewportParams *viewport,
+                             const QRect &dirtyRect,
+                             TextureColorizer *texColorizer );
+
+    virtual void setRepaintNeeded();
+
+ private:
+    class RenderJob;
+
+    void mapTexture( const ViewportParams *viewport, MapQuality mapQuality );
+
+    StackedTileLoader *const m_tileLoader;
+    bool m_repaintNeeded;
+    int m_radius;
+    QImage m_canvasImage;
+    QThreadPool m_threadPool;
+};
+
+}
+
+#endif
