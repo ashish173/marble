af0ba82b0d97f7f68c2011f7d9e3267c89945747
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2012-12-26 23:06:47 +0100

    adapt PlacemarkLayout

--- src/lib/PlacemarkLayout.cpp
+++ src/lib/PlacemarkLayout.cpp
@@ -320,6 +320,10 @@ void PlacemarkLayout::resetCacheData()
 
 QSet<TileId> PlacemarkLayout::visibleTiles( const ViewportParams *viewport ) const
 {
+    if ( m_styleResetRequested ) {
+        const_cast<PlacemarkLayout *>( this )->styleReset();
+    }
+
     int zoomLevel = qLn( viewport->radius() *4 / 256 ) / qLn( 2.0 );
 
     /**
@@ -375,6 +379,13 @@ QSet<TileId> PlacemarkLayout::visibleTiles( const ViewportParams *viewport ) con
             }
         }
     }
+
+    int numPlacemarks = 0;
+    foreach ( const TileId &id, tileIdSet ) {
+        numPlacemarks += m_placemarkCache.value( id ).size();
+    }
+
+    m_runtimeTrace = QString( "Placemarks: %1" ).arg( numPlacemarks );
 
     return tileIdSet;
 }
@@ -538,6 +549,11 @@ QVector<VisiblePlacemark *> PlacemarkLayout::generateLayout( const ViewportParam
 
     m_runtimeTrace = QString("Visible: %1 Drawn: %2").arg( placemarkList.count() ).arg( m_paintOrder.size() );
     return m_paintOrder;
+}
+
+QList<const GeoDataPlacemark *> PlacemarkLayout::tile(const TileId &id) const
+{
+    return m_placemarkCache.value( id );
 }
 
 QString PlacemarkLayout::runtimeTrace() const
